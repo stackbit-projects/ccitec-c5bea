15ed501d35a3f4926176b067f92d70a08fe2c929
Stackbit Code Editor <projects@stackbit.com>
2021-08-05 22:24:04 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -17,8 +17,8 @@ export default class SectionHero extends React.Component {
         return (
             
                 <div >
-                    <div style={{width:'100%'}}>
-                        <img src='https://vvsir.com/wp-content/uploads/2020/12/ingenieria-electrica-19.jpg'/>
+                    <div style={{width:'50%'}}>
+                        <img src={withPrefix(image)} alt={imageAlt} />
                     </div>
                 {/*
                     <div className="grid">
