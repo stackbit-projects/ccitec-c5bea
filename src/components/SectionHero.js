7fdd3ce28de772867173dd5850cb501970107698
Stackbit Code Editor <projects@stackbit.com>
2021-08-05 22:28:47 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -16,7 +16,7 @@ export default class SectionHero extends React.Component {
 
         return (
             <section id={sectionId} className="block hero-block bg-accent outer">
-                <div>
+                {/*<div>*/}
                     <div style={{width:'100%'}}>
                          <img src={withPrefix(image)} alt={imageAlt} />
                     </div>
