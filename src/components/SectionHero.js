0a75bccb3b0638648ad2651eb284512955596fea
Stackbit Code Editor <projects@stackbit.com>
2021-08-05 22:55:41 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -15,7 +15,7 @@ export default class SectionHero extends React.Component {
         const actions = _.get(section, 'actions');
 
         return (
-            <section id={sectionId} className="hero-block bg-accent outer">
+            <section id={sectionId} className="block hero-block bg-accent outer">
                                     
                     <div className="grid">
                         {image && (
